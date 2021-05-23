ee3fee3114e5be2a95b6f157b3f033435ca2f265
cholezooza <66155957+cholezooza@users.noreply.github.com>
2021-05-23 15:07:46 +0700

    Create mcot.php

---
+++ mcot.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <title>Live Streaming</title>
+   </head>
+   <body>
+      <h1>MCOT HD</h>
+      <br />
+<center>
+      <iframe width="852" height="480" src="https://www.dailymotion.com/embed/video/x74wlgj?autoplay=1" frameborder="0" allowfullscreen></iframe>
+      </iframe>
+</center>
+   </body>
+</html>
